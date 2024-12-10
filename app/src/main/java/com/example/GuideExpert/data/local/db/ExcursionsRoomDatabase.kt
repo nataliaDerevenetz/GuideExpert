bea34ddbd36dba7ef4e0e201a377336d54a1aefd
Natalia Derevenetz <nna911@inbox.ru>
2024-12-10 14:55:22 +0300

    update

--- app/src/main/java/com/example/GuideExpert/data/local/db/ExcursionsRoomDatabase.kt
+++ app/src/main/java/com/example/GuideExpert/data/local/db/ExcursionsRoomDatabase.kt
@@ -6,7 +6,7 @@ import com.example.GuideExpert.data.local.dao.ExcursionDao
 import com.example.GuideExpert.data.local.models.ExcursionEntity
 
 
-@Database(entities = [ExcursionEntity::class], version = 1)
+@Database(entities = [ExcursionEntity::class], version = 1,exportSchema = false)
 abstract class ExcursionsRoomDatabase: RoomDatabase() {
     abstract fun excursionDao() : ExcursionDao
 }
